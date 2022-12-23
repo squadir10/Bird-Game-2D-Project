cb702c287be1c2fd412f43bfe5c36758788749b8
Sabrina Quadir <97366704+squadir10@users.noreply.github.com>
2022-12-23 00:27:13 -0500

    Better physics
    smoother gameplay and lots of changing numbers back and forth. Im a genius.

--- Bird game/Assets/Scripts/Bird.cs
+++ Bird game/Assets/Scripts/Bird.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
-    // Start is called before the first frame update
-
     public Rigidbody2D myRigidbody2D;
+    public float flapStrength;
     
     void Start()
     {
@@ -19,10 +18,10 @@ public class Bird : MonoBehaviour
         // when spacebar is hit, make the bird fly 
         if (Input.GetKeyDown(KeyCode.Space) == true)
         {
-            myRigidbody2D.velocity = Vector2.up * 10; //bird flies up on y-axis by a force of 10 
+            myRigidbody2D.velocity = Vector2.up * flapStrength; //bird flies up on y-axis by a force of 10 
         }
         // changes the rotation of the bird when it comes down for a more appealing affect.
-        transform.eulerAngles = new Vector3(0, 0, myRigidbody2D.velocity.y * 4f); 
+        transform.eulerAngles = new Vector3(0, 0, myRigidbody2D.velocity.y * 2f); 
 
 
     }
